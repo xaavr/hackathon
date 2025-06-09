0920af82362fd01ebf0450dec3976d54ce201029
xavier <xaviercarnold@gmail.com>
2025-06-09 13:49:07 +0000

    backend added

---
+++ backend/src/main/java/model/StockHolding.java
@@ -0,0 +1,17 @@
+package model;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.ManyToOne;
+
+@Entity
+public class StockHolding {
+    @Id @GeneratedValue Long id;
+    String symbol;
+    int shares;
+    double avgPrice;
+    @ManyToOne User owner;
+    String portfolioName; // New field to associate with a portfolio
+
+    public double getTotalValue() {
+        return shares * avgPrice; // Method to calculate total value
+    }
+}
